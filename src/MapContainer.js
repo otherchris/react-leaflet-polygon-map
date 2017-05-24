de44a45d830b668ffdbefe1360cb4d308bbf8e64
Chris Caragianis <chriscaragianis@gmail.com>
2017-05-24 09:37:59 -0400

    clear linting

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -18,63 +18,52 @@ import MapExample from 'map-example';
 import { transformEncodedIntoGeojson } from 'transform-to-geojson';
 import PropTypes from 'prop-types';
 
-/*export default function Geojsonify(Component) {
-  const klass =
-*/
 class MapContainer extends React.Component {
-    constructor(props) {
-      super(props);
-      // we want to output geojson (if the props translate to it)
-      // sometims this is kindof expensive, so we can cache in state
-      // but we don't HAVE to
-      this.state = { geojsons: null };
-      this.mapPropsToState = this.mapPropsToState.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    // we want to output geojson (if the props translate to it)
+    // sometimes this is kind of expensive, so we can cache in state
+    // but we don't HAVE to
+    this.state = { geojsons: null };
+    this.mapPropsToState = this.mapPropsToState.bind(this);
+  }
 
-    componentWillMount() {
-      this.mapPropsToState(this.props);
-    }
+  componentWillMount() {
+    this.mapPropsToState(this.props);
+  }
 
-    componentWillReceiveProps(nextProps) {
-      this.mapPropsToState(nextProps);
-    }
+  componentWillReceiveProps(nextProps) {
+    this.mapPropsToState(nextProps);
+  }
 
-    // transform whatever we get from props into internal component state
-    // this is bascially a "cache" of the tranformed data
-    mapPropsToState(props) {
-      // TODO take whatever inputs, and construct valid geojson
-      // NOTE this can be better done outside of React components, perhaps a utility library
-      // eg: extend(geojson, transformPolygonsIntoGeojson(props.polygons))
-      if(this.props.geozips.every(entry => (entry.path)))
-      {
-        const geojsons = _.map(this.props.geozips, (geozip) => {
-            const result = transformEncodedIntoGeojson(geozip.path);
-            result.properties.zip = geozip.zip;
-            return result;
-          });
+  // transform whatever we get from props into internal component state
+  // this is bascially a "cache" of the tranformed data
+  mapPropsToState(props) {
+    // TODO take whatever inputs, and construct valid geojson
+    // NOTE this can be better done outside of React components, perhaps a utility library
+    // eg: extend(geojson, transformPolygonsIntoGeojson(props.polygons))
+    if (this.props.geozips.every(entry => (entry.path))) {
+      const geojsons = _.map(this.props.geozips, (geozip) => {
+        const result = transformEncodedIntoGeojson(geozip.path);
+        result.properties.zip = geozip.zip;
+        return result;
+      });
 
-        if (!isEqual(geojsons, this.state.geojsons)) {
-          this.setState({ geojsons });
-        }
+      if (!isEqual(geojsons, this.state.geojsons)) {
+        this.setState({ geojsons });
       }
-      //if (Object.keys(geojson).length !== 0) {
-        // this is kinda expensive, only do this if changes found
-        // if (!isEqual(geojsons, this.state.geojsons)) {
-        //   this.setState({ geojson: geojsons });
-        // }
-      // }
     }
+  }
 
-    render() {
-      return (
-        <MapExample geojsons={this.state.geojsons} />
-      );
-    }
-};
-  // klass.displayName = `geojsonify(${Component.name})`;
-  // return klass;
+  render() {
+    return (
+      <MapExample geojsons={this.state.geojsons} />
+    );
+  }
+}
+
 MapContainer.propTypes = {
-    geozips: PropTypes.arrayOf(PropTypes.object)
+  geozips: PropTypes.arrayOf(PropTypes.object),
 };
 
 export default MapContainer;
