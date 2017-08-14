81400bd2b7d6cba528d8a2637338f857cfc2cab2
Chris Caragianis <chriscaragianis@gmail.com>
2017-08-14 11:20:20 -0400

    move to create-react-app

--- stories/poly.js
+++
@@ -1,3 +0,0 @@
-const poly = 'eiphFnhojOif@cn@oQgRLOzZ{z@n@gBjCce@CM~f@{SnA{@cBs[~K~G~E{Czc@nEfd@lEjCaKa@tKnGl@qDzJyBzq@fBjP~Jt@{@fPyFk@~@lToJ_AWlIhJ~@_A`S{Ii@sArRmI{@bAsRcGm@b@iIuLmAc@hI_KaA`@tIaFg@gAtSmDpHkTwBc@hI{Go@wKpQiBcB';
-export default poly;
-
