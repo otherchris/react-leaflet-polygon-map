f580687964429f00c43fada609f78a1df83155c4
Chris Caragianis <chriscaragianis@gmail.com>
2017-08-15 10:22:36 -0400

    stories for multiple poly formats

--- src/stories/poly.js
+++ src/stories/poly.js
@@ -1,3 +1,29 @@
-const poly = 'eiphFnhojOif@cn@oQgRLOzZ{z@n@gBjCce@CM~f@{SnA{@cBs[~K~G~E{Czc@nEfd@lEjCaKa@tKnGl@qDzJyBzq@fBjP~Jt@{@fPyFk@~@lToJ_AWlIhJ~@_A`S{Ii@sArRmI{@bAsRcGm@b@iIuLmAc@hI_KaA`@tIaFg@gAtSmDpHkTwBc@hI{Go@wKpQiBcB';
+const poly = {
+  polyline: 'eiphFnhojOif@cn@oQgRLOzZ{z@n@gBjCce@CM~f@{SnA{@cBs[~K~G~E{Czc@nEfd@lEjCaKa@tKnGl@qDzJyBzq@fBjP~Jt@{@fPyFk@~@lToJ_AWlIhJ~@_A`S{Ii@sArRmI{@bAsRcGm@b@iIuLmAc@hI_KaA`@tIaFg@gAtSmDpHkTwBc@hI{Go@wKpQiBcB',
+  geoJSON: {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [
+            [
+              [ -85.76820373535156, 38.23966324024717 ],
+              [ -85.75429916381836, 38.22793227923281 ],
+              [ -85.72528839111328, 38.23022468473982 ],
+              [ -85.74125289916992, 38.253279568348304 ],
+              [ -85.76820373535156, 38.23966324024717 ]
+            ]
+          ]
+        }
+      }
+    ]
+  },
+  wkt: 'POLYGON ((-85.76820373535156 38.23966324024717, -85.75429916381836 38.22793227923281, -85.72528839111328 38.23022468473982, -85.74125289916992 38.253279568348304, -85.76820373535156 38.23966324024717))',
+
+};
+
 export default poly;
 
