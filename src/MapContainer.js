a918724d72fc086ec1a0af9abe4cd7239d8d897b
Chris Caragianis <chriscaragianis@gmail.com>
2017-08-15 11:22:53 -0400

    tileset options

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -22,13 +22,30 @@ class MapContainer extends React.Component {
       points: this.props.points,
     });
   }
+  getStyle(str) {
+    const styles = {
+      'default': {
+        tileset: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      },
+      'minimal_light': {
+        tileset: 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png',
+      },
+      'minimal_dark': {
+        tileset: 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png',
+      },
+    }
+    return styles[str];
+  }
   render() {
-    const { tileLayerProps, width, height, zoom, center } = this.props;
+    const { tileLayerProps, width, height, zoom, center, style } = this.props;
+    const sty = this.getStyle(style);
     return (
       <MapExample
         polygons={this.state.polygons}
         points={this.state.points}
-        tileLayerProps={tileLayerProps}
+        tileLayerProps={{
+          url: sty.tileset
+        }}
         height={height}
         zoom={zoom}
         center={center}
@@ -39,7 +56,13 @@ class MapContainer extends React.Component {
 
 MapContainer.propTypes = {
   polygons: PropTypes.arrayOf(PropTypes.object),
+  polygons: PropTypes.arrayOf(PropTypes.array),
+  style: PropTypes.string,
   height: PropTypes.number,
 };
+
+MapContainer.defaultProps = {
+  style: 'default',
+}
 export default MapContainer;
 
