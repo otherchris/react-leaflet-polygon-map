e4998191baa256c84450fbf7072d59317cfc31b2
Micah Potts <mrs.micahpotts@gmail.com>
2017-08-25 15:05:40 -0400

    Fixture data format complete minus coordinates (to be added after code completion)

--- src/stories/poly.js
+++ src/stories/poly.js
@@ -26,13 +26,10 @@ const poly = {
   wkt: { type: 'wkt',
     // eslint-disable-next-line max-len
     data: 'POLYGON ((-85.76837539672852 38.24438205858283, -85.78210830688477 38.22442610753021, -85.75052261352539 38.236157634068825, -85.7567024230957 38.21660403859855, -85.72151184082031 38.23858461019401, -85.73284149169922 38.25368397473024, -85.75824737548828 38.24222492249137, -85.76837539672852 38.24438205858283))' },
-  wkbHex: { type: 'wkb',
-    // eslint-disable-next-line max-len
-    data: '0103000000010000000700000000000000117055c026043c75e7224340ffffff8f167155c0f771b23429204340ffffffef9a6e55c0334dd1e16f204340000000800b6e55c068ddbf228f22434000000070846e55c00b3c634d2e254340000000302d6f55c071c4963b9c21434000000000117055c026043c75e7224340' },
   wkb: { type: 'wkb',
     // eslint-disable-next-line max-len
     data: 'AQMAAAABAAAABwAAAAAAAAARcFXAJgQ8deciQ0D///+PFnFVwPdxsjQpIENA////75puVcAzTdHhbyBDQAAAAIALblXAaN2/Io8iQ0AAAABwhG5VwAs8Y00uJUNAAAAAMC1vVcBxxJY7nCFDQAAAAAARcFXAJgQ8deciQ0A=' },
-  circle: { type: 'circle',
+  circle: { type: 'Circle',
     data: {
       center:
       { lat: 38.25850297757688, lng: -85.7552433013916 },
@@ -52,7 +49,7 @@ const poly = {
       area: 213839.345998646,
     },
   },
-  rectangle: { type: 'rectangle',
+  rectangle: { type: 'Rectangle',
     data: {
       bounds: { south: 38.25728983213629,
         west: -85.76575756072998,
@@ -68,4 +65,3 @@ const poly = {
 };
 
 export default poly;
-
