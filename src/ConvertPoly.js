6f68a9b6367483ba86ee4b07169b10f2fbe202aa
Micah Potts <mrs.micahpotts@gmail.com>
2017-08-25 13:10:18 -0400

    Composed fixture data and got geoJSON section of test to run

--- src/ConvertPoly.js
+++ src/ConvertPoly.js
@@ -1,77 +1,76 @@
-//convertpoly function takes polyline, geoJSON, wkt, wkb, wkbHex, circle paths,
-//and rectangle paths and returns geoJSON
+// convertpoly function takes polyline, geoJSON, wkt, wkb, wkbHex, circle paths,
+// and rectangle paths and returns geoJSON
 import wkx from 'wkx';
-var wkx = require('wkx');
 import map from 'lodash/map';
 import polyline from 'polyline';
 
 const convertPoly = (poly) => {
+  console.log(poly.data);
   switch (poly.type) {
-    case 'polyline':
-      return {
-        "type": "FeatureCollection",
-        "features": [{
-          "type": "Feature",
-          "properties": {},
-          "geometry": polyline.toGeoJSON(poly.data)
-        }]
-      };
-      break;
-    case 'geoJSON':
-      return poly.data;
-      break;
-    case 'wkt':
-      return {
-        "type": "FeatureCollection",
-        "features": [{
-          "type": "Feature",
-          "properties": {},
-          "geometry": wkx.Geometry.parse(poly.data).toGeoJSON()
-        }]
-      };
-      break;
-    case 'wkb':
-      const encoding = poly.encoding ||'base64';
-      const buf = Buffer.from(poly, encoding);
-      return {
-        "type": "FeatureCollection",
-        "features": [{
-          "type": "Feature",
-          "properties": {},
-          "geometry": wkx.Geometry.parse(buf).toGeoJSON(poly.data)
-        }]
-      };
-      break;
-    case 'circle':
-      const circleCoords = map(circle.data.path, (x) => [x.lng, x.lat]);
-      return {
-        "type": "FeatureCollection",
-        "features": [{
-          "type": "Feature",
-          "properties": {},
-          "geometry": {
-            "type": "Polygon",
-            "coordinates": wkx.Geometry.parse(circleCoords).toGeoJSON // circleCoords.parse(poly.data).toGeoJSON()
-          }
-        }]
-      };
-      break;
-    case 'rectangle':
-      const rectCoords = map(rectangle.path, (x) => [x.lng, x.lat]);
-      return {
-        "type": "FeatureCollection",
-        "features": [{
-          "type": "Feature",
-          "properties": {},
-          "geometry": rectCoords.parse(poly.data).toGeoJSON()
-        }]
-      };
-      break;
+  case 'polyline':
+  const a = {
+    type: 'FeatureCollection',
+    features: [{
+      type: 'Feature',
+      properties: {},
+      geometry: polyline.toGeoJSON(poly.data),
+    }],
+  };
+    console.log('polyline results', a);
+    return a;
+  case 'wkt':
+    return {
+      type: 'FeatureCollection',
+      features: [{
+        type: 'Feature',
+        properties: {},
+        geometry: wkx.Geometry.parse(poly.data).toGeoJSON(),
+      }],
+    };
+  case 'wkb':
+  const buf = Buffer.from(poly.data, 'base64');
+    return {
+      type: 'FeatureCollection',
+      features: [{
+        type: 'Feature',
+        properties: {},
+        geometry: wkx.Geometry.parse(buf).toGeoJSON(poly.data),
+      }],
+    };
+  case 'circle':
+  const circleCoords = map(poly.circle.data.path, (x) => [x.lng, x.lat]);
+    return {
+      type: 'FeatureCollection',
+      features: [{
+        type: 'Feature',
+        properties: {},
+        geometry: {
+          type: 'Polygon',
+          coordinates: wkx.Geometry.parse(circleCoords).toGeoJSON(),
+        },
+      }],
+    };
+  case 'rectangle':
+  const rectCoords = map(poly.rectangle.path, (x) => [x.lng, x.lat]);
+    return {
+      type: 'FeatureCollection',
+      features: [{
+        type: 'Feature',
+        properties: {},
+        geometry: rectCoords.parse(poly.data).toGeoJSON(),
+      }],
+    };
+  case 'geoJSON':
+    return poly.data;
+  default:
+    return poly.data;
   }
+  // return null;
 };
 
-// export default convertPoly;
+export default convertPoly;
 
-const exampleGeoJson = "geoJSON: {type: 'geoJSON', data: {"type": "FeatureCollection", "features": [{"type": "Feature", "properties": {}, "geometry": {"type": "Polygon", "coordinates": [[[ -85.76820373535156, 38.23966324024717 ], [ -85.75429916381836, 38.22793227923281 ], [ -85.72528839111328, 38.23022468473982 ], [ -85.74125289916992, 38.253279568348304 ], [ -85.76820373535156, 38.23966324024717 ]]]}}]}}"
+// eslint-disable-next-line max-len
+// const exampleGeoJson = 'geoJSON: {type: 'geoJSON', data: {'type': 'FeatureCollection', 'features': [{'type': 'Feature', 'properties': {}, 'geometry': {'type': 'Polygon', 'coordinates': [[[ -85.76820373535156, 38.23966324024717 ], [ -85.75429916381836, 38.22793227923281 ], [ -85.72528839111328, 38.23022468473982 ], [ -85.74125289916992, 38.253279568348304 ], [ -85.76820373535156, 38.23966324024717 ]]]}}]}}'
 
-console.log(convertPoly(geoJSON));
+// console.log(convertPoly(geoJSON));
