ffa23c5f9e516c167ecf998d6a47dcfd6f39e3a6
Chris Caragianis <chriscaragianis@gmail.com>
2017-09-18 10:25:14 -0400

    add control function props

--- src/ZipRadiusControl.js
+++ src/ZipRadiusControl.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class ZipRadiusControl extends React.Component {
   constructor(props) {
@@ -9,12 +10,18 @@ class ZipRadiusControl extends React.Component {
     return (
       <div>
         <label labelFor="radius">Radius</label>
-        <input type="text" id="radius"></input>
+        <input type="text" id="radius" onChange={this.props.zipRadiusChange}></input>
         <span className="center">{this.props.center || 'center'}</span>
-        <button>Sumbit</button>
+        <button onClick={this.props.zipRadiusClick}>Get Zips</button>
       </div>
     );
   }
+}
+
+ZipRadiusControl.propTypes = {
+  center: PropTypes.string,
+  zipRadiusClick: PropTypes.function,
+  zipRadiusChange: PropTypes.function,
 }
 
 export default ZipRadiusControl;
