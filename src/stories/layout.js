127746b5f991b9f2c08b5807f06c030cfdf0390c
Chris Caragianis <chriscaragianis@gmail.com>
2017-09-19 16:42:01 -0400

    stories and checklists for current tasks'

--- src/stories/layout.js
+++ src/stories/layout.js
@@ -5,6 +5,11 @@ import MapContainerStoryWithNotes from './MapContainerStoryWithNotes';
 import points from './points';
 import dpPoly from './dpPoly';
 
+const DummyComponent = (props) => (
+  <div style={{ backgroundColor: 'red', width: '100%', height: '100%' }}>
+    A COMPONENT
+  </div>
+);
 
 storiesOf('Layout', module)
   .add('Default Layout', () => (
@@ -14,6 +19,14 @@ storiesOf('Layout', module)
         <input type="checkbox" /> 2. No zip/radius tools
         <input type="checkbox" /> 3. No extra lower-right component
         <input type="checkbox" /> 4. No edit tools
+      `}
+    />
+  ))
+  .add('With legend component', () => (
+    <MapContainerStoryWithNotes
+      legendComponent={(<DummyComponent/>)}
+      additionalNotes={`Should see \n
+        <input type="checkbox" /> 1. Red rectangle in the lower right
       `}
     />
   ));
