c18cbae3ce5abeae34eba9093399edb4b908c289
Chris Caragianis <chriscaragianis@gmail.com>
2017-09-25 10:15:16 -0400

    add area-related stories

--- src/stories/index.js
+++ src/stories/index.js
@@ -4,6 +4,7 @@ import 'leaflet-draw/dist/leaflet.draw.css';
 import MapContainerStoryWithNotes from './MapContainerStoryWithNotes';
 import points from './points';
 import dpPoly from './dpPoly';
+import poly from './poly';
 import '../main.css';
 import './layout';
 import './polygons';
@@ -34,6 +35,51 @@ storiesOf('onChange prop', module)
       }}
       additionalNotes={`Should see \n
         <input type="checkbox" /> 1. Alert with state on component update />
+      `}
+    />
+  ));
+
+storiesOf('handleSubmit', module)
+  .add('Do not show Submit button if handleSubmit prop not provided', () => (
+    <MapContainerStoryWithNotes
+      additionalNotes={`Should see \n
+        <input type="checkbox" /> 1. No submit button/>
+      `}
+    />
+  ))
+  .add('Show Submit button if handleSubmit prop is provided', () => (
+    <MapContainerStoryWithNotes
+      handleSubmitpol={(state) => {
+        alert(JSON.stringify(state, null, '  '));
+      }}
+      additionalNotes={`Should see \n
+        <input type="checkbox" /> 1. Submit button
+      `}
+    />
+  ))
+  .add('Submit should be disabled if any polygon is too big', () => (
+    <MapContainerStoryWithNotes
+      polygons={[poly.tooBigPoly]}
+      handleSubmitpol={(state) => {
+        alert(JSON.stringify(state, null, '  '));
+      }}
+      additionalNotes={`Should see \n
+        <input type="checkbox" /> 1. Submit button in disabled state
+        <input type="checkbox" /> 2. Visual indication that the poly is too big
+        <input type="checkbox" /> 3. Resize or delete poly to enable submit
+      `}
+    />
+  ))
+  .add('Submit should be disabled if sum of polygons is too big', () => (
+    <MapContainerStoryWithNotes
+      polygons={poly.tooBigPolyArray}
+      handleSubmitpol={(state) => {
+        alert(JSON.stringify(state, null, '  '));
+      }}
+      additionalNotes={`Should see \n
+        <input type="checkbox" /> 1. Submit button in disabled state
+        <input type="checkbox" /> 2. No visual indication on any given polygon
+        <input type="checkbox" /> 3. Resize or delete poly to enable submit
       `}
     />
   ));
