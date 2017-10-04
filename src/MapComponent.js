aca1c2dbc81a72d6486e5a38aec3bc34f6cdd7be
Chris Caragianis <chriscaragianis@gmail.com>
2017-10-04 10:30:59 -0400

    extract tooltip helpers

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -20,6 +20,13 @@ import {
 } from 'react-leaflet';
 import { EditControl } from 'react-leaflet-draw';
 import ZipRadiusControl from './ZipRadiusControl';
+import {
+  tooltipMessage,
+  circleTooltip,
+  rectTooltip,
+  pointsTooltip,
+  tooltipClass,
+} from './tooltipHelpers';
 import './main.css';
 import getArea from './getArea';
 
@@ -110,99 +117,10 @@ MapSubmitButton.propTypes = {
   handleSubmit: PropTypes.func,
   text: PropTypes.string,
 };
-const tooltipMessage = (polyProps, tooltipOptions) => {
-  if (tooltipOptions && tooltipOptions.includeArea) {
-    const unitName = tooltipOptions.units.name ? `Sq ${tooltipOptions.units.name}` : 'Sq Meters';
-    const convertedArea = tooltipOptions.units.conversion ?
-      polyProps.area * tooltipOptions.units.conversion :
-      polyProps.area;
-    const areaWithUnit = `${convertedArea.toFixed(4)} ${unitName}`;
-    const text = tooltipOptions.text ? tooltipOptions.text : '';
-    const tipMessage = `${text} ${areaWithUnit}`;
-    return tipMessage;
-  }
-  if (tooltipOptions && tooltipOptions.includeArea && !tooltipOptions.units) {
-    const area = polyProps.area;
-    const text = tooltipOptions.text ? tooltipOptions.text : '';
-    const tipMessage = `${text} ${area.toFixed(4)} Sq Meters`;
-    return tipMessage;
-  }
-  if (tooltipOptions && !(tooltipOptions.includeArea)) {
-    const text = tooltipOptions && tooltipOptions.text ? tooltipOptions.text : '';
-    const tipMessage = text;
-    return tipMessage;
-  }
-  const tipMessage = `${polyProps.area.toFixed(4)} Sq Meters`;
-  return tipMessage;
-};
-
-const circleTooltip = (circleProps, tooltipOptions) => {
-  if (tooltipOptions && tooltipOptions.includeArea && tooltipOptions.units) {
-    const unitName = tooltipOptions.units.name ? `Sq ${tooltipOptions.units.name}` : 'Sq Meters';
-    const convertedArea = tooltipOptions.units.conversion ?
-      circleProps.area * tooltipOptions.units.conversion : circleProps.area;
-    const areaWithUnit = `${convertedArea.toFixed(4)} ${unitName}`;
-    const text = tooltipOptions.text ? tooltipOptions.text : '';
-    const tipMessage = `${text} ${areaWithUnit}`;
-    return tipMessage;
-  }
-  if (tooltipOptions && tooltipOptions.includeArea && !tooltipOptions.units) {
-    const area = circleProps.area;
-    const text = tooltipOptions.text ? tooltipOptions.text : '';
-    const tipMessage = `${text} ${area.toFixed(4)} Sq Meters`;
-    return tipMessage;
-  }
-  if (tooltipOptions && !(tooltipOptions.includeArea)) {
-    const text = tooltipOptions && tooltipOptions.text ? tooltipOptions.text : '';
-    const tipMessage = text;
-    return tipMessage;
-  }
-  const tipMessage = `${circleProps.area.toFixed(4)} Sq Meters`;
-  return tipMessage;
-};
-const rectTooltip = (rectProps, tooltipOptions) => {
-  if (tooltipOptions && tooltipOptions.includeArea) {
-    const noArea = 'Area cannot be calculated on rectangle';
-    const text = tooltipOptions.text ? tooltipOptions.text : '';
-    const tipMessage = `${text} ${noArea}`;
-    return tipMessage;
-  }
-  if (tooltipOptions && !(tooltipOptions.includeArea)) {
-    const text = tooltipOptions && tooltipOptions.text ? tooltipOptions.text : '';
-    const tipMessage = text;
-    return tipMessage;
-  }
-  const tipMessage = 'Area cannot be calculated on rectangle';
-  return tipMessage;
-};
-const pointsTooltip = (point, tooltipOptions) => {
-  const coords = point.geometry.coordinates;
-  if (tooltipOptions && tooltipOptions.marker && tooltipOptions.marker.includeLocation) {
-    const latLng = `${coords[1].toFixed(4)}, ${coords[0].toFixed(4)}`;
-    const text = tooltipOptions.marker.text ? tooltipOptions.marker.text : '';
-    const tipOpts = `${latLng} ${text}`;
-    return tipOpts;
-  }
-  if (tooltipOptions && tooltipOptions.marker && !(tooltipOptions.marker.includeLocation)) {
-    const text = tooltipOptions.marker.text ? tooltipOptions.marker.text : '';
-    const tipOpts = `${text}`;
-    return tipOpts;
-  }
-  const latLng = `${coords[1].toFixed(4)}, ${coords[0].toFixed(4)}`;
-  return latLng;
-};
-const tooltipClass = (tooltipOptions) => {
-  if (tooltipOptions && tooltipOptions.className) {
-    const tipClass = tooltipOptions.className ? `${tooltipOptions.className}` : '';
-    return tipClass;
-  }
-  return 'tooltipClass';
-};
 const MapComponent = (props) => {
   const { zoom, tileLayerProps, center, height, includeZipRadius, tooltipOptions } = props;
   merge(style, props.style);
   merge(hoveredStyle, props.hoveredStyle);
-  // const polyWithArea = map(props.polygons, getArea);
   const polygons = map(props.polygons, (result, index) => {
     const p = result.properties;
     return (
