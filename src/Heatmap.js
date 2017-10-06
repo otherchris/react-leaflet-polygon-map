5d8ab92332f25afa1621b0a9c1ab20d1265e8e80
Micah Potts <mrs.micahpotts@gmail.com>
2017-10-06 14:47:11 -0400

    We have a heatmap

---
+++ src/Heatmap.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { render } from 'react-dom';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import HeatmapLayer from 'react-leaflet-heatmap-layer';
+import { addressPoints } from './stories/realworld.10000.js';
+
+const Heatmap = (props) => {
+  return (
+    <HeatmapLayer
+      fitBoundsOnLoad
+      fitBoundsOnUpdate
+      points={props.heatmap}
+      longitudeExtractor={m => m[1]}
+      latitudeExtractor={m => m[0]}
+      intensityExtractor={m => parseFloat(m[2])}
+    />
+  );
+};
+
+Heatmap.propTypes = {
+  points: PropTypes.array,
+  heatmap: PropTypes.object,
+};
+
+export default Heatmap;
