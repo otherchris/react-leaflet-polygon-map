6188570f2626df60e3b9ef5323f87c403c650060
Chris Caragianis <chriscaragianis@gmail.com>
2017-10-19 09:19:45 -0400

    revert react widgets on circle approx

--- src/CircleApprox.js
+++ src/CircleApprox.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { NumberPicker } from 'react-widgets';
-import localizer from 'react-widgets-simple-number';
-
-localizer();
 
 const CircleApprox = (props) => (
   <div className="circle-approx">
     Radius (in miles)
-    <NumberPicker
+    <input
+      type="text"
       onChange={props.radiusChange}
-      max={10}
-      min={0}
-      defaultValue={0}
-     />
+    />
     <button onClick={props.makeCircle}>
       Create Circle
     </button>
