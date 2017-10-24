16c6324e8b27b9148c705322d67a3fdcb1bef515
Chris Caragianis <chriscaragianis@gmail.com>
2017-10-24 09:55:30 -0400

    change react-widgets version for portal. breaks storybook, need to revisit that

--- src/CircleApprox.js
+++ src/CircleApprox.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { NumberPicker } from 'react-widgets';
-import numberLocalizer from 'react-widgets/lib/localizers/simple-number';
+import numberLocalizer from 'react-widgets-simple-number';
 
 numberLocalizer();
 
