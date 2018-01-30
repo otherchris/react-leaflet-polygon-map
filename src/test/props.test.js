833d17c48bafa7d36d431361ac4bb211c8c2d6cd
Chris Caragianis <chriscaragianis@gmail.com>
2018-01-30 09:01:56 -0500

    WIP makeCenter tests

--- src/test/props.test.js
+++ src/test/props.test.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import MapContainer from '../MapContainer';
-import { shallow } from 'enzyme';
+import { shallow, mount, render } from 'enzyme';
+import map from 'lodash/map';
+import fill from 'lodash/fill';
+import MapContainer, { makeCenter } from '../MapContainer';
 import testPropType from './support/testPropType';
 
 describe('apikey', () => {
   testPropType(MapContainer, 'apikey', 'string');
+});
+
+describe('center', () => {
+  it('makeCenter', () => {
+    const array = [1, -1];
+    const none = null;
+    const latLng = { lat: 1, lng: -1 };
+    const geoJSON = { type: "Point", coordinates: [-1, 1] };
+    const geoJSONFeature = { type: "Feature", geometry: geoJSON };
+
+    expect(map([array, geoJSON, geoJSONFeature, latLng])).toEqual(fill(Array(4), geoJSON))
+  });
+
+  it('can be supplied as an array', () => {
+    const wrapper = mount(<MapContainer center={[1,-1]} />);
+    expect(wrapper.state().center).toEqual({ type: 'Point', coordinates: [-1, 1] })
+  });
 });
 
