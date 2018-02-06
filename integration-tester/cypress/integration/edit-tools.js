2a4ec7b08b58d3c9b6ed97a4ce1dc6a5069e234e
Chris Caragianis <chriscaragianis@gmail.com>
2018-02-06 12:08:11 -0500

    fixed cross origin problem, running cyress tests

---
+++ integration-tester/cypress/integration/edit-tools.js
@@ -0,0 +1,14 @@
+describe('Edit Tools', () => {
+  it('shows the edit tools', () => {
+    cy.visit('http://127.0.0.1:5000');
+
+    cy.get('#edit-tools').click();
+
+    cy.get('a.leaflet-draw-draw-polygon');
+    cy.get('a.leaflet-draw-draw-rectangle');
+    cy.get('a.leaflet-draw-draw-marker');
+
+    cy.get('#default').click();
+
+  })
+});
