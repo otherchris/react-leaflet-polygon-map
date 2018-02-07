097d7aca7d1063dd702450c82e6dd42d13b7a00e
Chris Caragianis <chriscaragianis@gmail.com>
2018-02-07 16:43:21 -0500

    fix weird test inconsistency

--- integration-tester/cypress/integration/zoom.js
+++ integration-tester/cypress/integration/zoom.js
@@ -1,8 +1,8 @@
 describe('Zoom', () => {
   it('starts zoomed to shapes', () => {
     cy.visit('localhost:3000');
-
-    cy.get('#large-poly').click();
+    cy.wait(500)
+    cy.get('#large-poly').click('center');
     cy.get('img[src*="m&x=192&y=401&z=10"]');
   });
 });
