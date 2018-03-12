d48de445c4c5215017897c50a3b995ca76091d95
Chris Caragianis <chriscaragianis@gmail.com>
2018-03-12 11:09:33 -0400

    remove outdated unit tests

--- src/test/cleanProps.test.js
+++
@@ -1,9 +0,0 @@
-import { cleanPropsFunc } from '../cleanProps';
-import noop from 'lodash/noop';
-
-describe('cleanProps', () => {
-  const extUpdate = (p, cb) => p;
-  it('passes props through', () => {
-    expect(cleanPropsFunc({a: 'a'})).toEqual({a: 'a'});
-  });
-});
