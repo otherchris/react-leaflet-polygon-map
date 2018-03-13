c8d6c50497525f76b0375fa8af8b5612ec3fe62a
Chris Caragianis <chriscaragianis@gmail.com>
2018-03-13 08:14:02 -0400

    clear old test files

--- test-old/MapContainer.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-// import { shallow, mount, render } from 'enzyme';
-// import MapContainer from '../MapContainer';
-
-test('get tiles url', () => {
-//   const wrapper = shallow(<MapContainer />);
-//   expect(wrapper.getTilesUrl('minimal_light')).toBe('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png');
-});
