0eca5c67d93e27e323915d51e5703e251401f630
Chris Caragianis <chriscaragianis@gmail.com>
2018-03-15 08:57:44 -0400

    Cluster test stubs

---
+++ integration-tester/cypress/integration/clusters.js
@@ -0,0 +1,11 @@
+describe('Clusters', () => {
+  it(`Given a large set of points,
+  When the map is zoomed out,
+  Then points are collected into clusters`, () => {
+  });
+
+  it(`Given a set of clusters,
+  When the map is zoomed in,
+  Cluster size is reduced`, () => {
+  });
+});
