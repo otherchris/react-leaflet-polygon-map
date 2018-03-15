84eed2bd49555aeefede59d9b858d6c3b2394c7f
Chris Caragianis <chriscaragianis@gmail.com>
2018-03-15 09:00:00 -0400

    Stub for negtaive case in clustering

--- integration-tester/cypress/integration/clusters.js
+++ integration-tester/cypress/integration/clusters.js
@@ -1,10 +1,19 @@
 describe('Clusters', () => {
+
   it(`Given a large set of points,
+  And clustering prop is false,
+  When the map is displayed,
+  Then points are not collected into clusters`, () => {
+  });
+
+  it(`Given a large set of points,
+  And clustering prop is true,
   When the map is zoomed out,
   Then points are collected into clusters`, () => {
   });
 
   it(`Given a set of clusters,
+  And clustering prop is true,
   When the map is zoomed in,
   Cluster size is reduced`, () => {
   });
